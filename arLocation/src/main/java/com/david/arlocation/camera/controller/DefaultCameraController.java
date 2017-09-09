8026a12c4d2a0ed8b3ad0eaaa35d1503d3afd2cf
santukis <santamadavid@gmail.com>
2017-09-09 13:00:38 +0200

    orientation listener implementation

--- arLocation/src/main/java/com/david/arlocation/camera/controller/DefaultCameraController.java
+++ arLocation/src/main/java/com/david/arlocation/camera/controller/DefaultCameraController.java
@@ -5,9 +5,11 @@ import android.content.Context;
 import android.content.res.Configuration;
 import android.graphics.SurfaceTexture;
 import android.hardware.Camera;
+import android.hardware.SensorManager;
 import android.opengl.Matrix;
 import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
+import android.view.OrientationEventListener;
 import android.view.TextureView;
 
 import com.david.arlocation.camera.boundary.CameraController;
@@ -22,8 +24,9 @@ public class DefaultCameraController extends Observable implements CameraControl
     private Camera camera;
 
     private int cameraOrientation;
+    private OrientationEventListener orientationListener;
 
-    private TextureView textureView;
+    private TextureView cameraView;
 
     private float[] projectionMatrix = new float[16];
 
@@ -31,25 +34,51 @@ public class DefaultCameraController extends Observable implements CameraControl
                                    @NonNull Observer observer,
                                    @NonNull TextureView textureView) {
         addObserver(observer);
-        this.textureView = textureView;
-        setCameraOrientation(context);
+        cameraView = textureView;
+        initializeOrientationListener(context);
     }
 
-    private void setCameraOrientation(Context context) {
-        final int PORTRAIT = 90;
-        final int LANDSCAPE = 180;
+    private void initializeOrientationListener(@NonNull final Context context) {
+        orientationListener = new OrientationEventListener(context, SensorManager.SENSOR_DELAY_NORMAL) {
+            @Override
+            public void onOrientationChanged(int orientation) {
+                if(orientation == ORIENTATION_UNKNOWN) {
+                    return;
+                }
 
-        cameraOrientation = PORTRAIT;
+                int degrees = 270;
+                if (orientation < 45 || orientation > 315)
+                    degrees = 0;
+                else if (orientation < 135)
+                    degrees = 90;
+                else if (orientation < 225)
+                    degrees = 180;
 
-        if (context.getResources().getConfiguration().orientation
-                == Configuration.ORIENTATION_LANDSCAPE) {
-            cameraOrientation = LANDSCAPE;
+                setCameraOrientation(degrees);
+            }
+        };
+    }
+
+    private void setCameraOrientation(int degrees) {
+        Camera.CameraInfo info = new Camera.CameraInfo();
+        Camera.getCameraInfo(Camera.CameraInfo.CAMERA_FACING_BACK, info);
+
+        if(info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT){
+            this.cameraOrientation = (info.orientation + degrees) % 360;
+            this.cameraOrientation =  (360 - this.cameraOrientation) % 360;
+
+        } else {
+            this.cameraOrientation = (info.orientation - degrees + 360) % 360;
         }
     }
 
     @Override
     public void connectCamera() {
-        textureView.setSurfaceTextureListener(this);
+        cameraView.setSurfaceTextureListener(this);
+
+        if(orientationListener != null) {
+            orientationListener.enable();
+        }
     }
 
     @Override
@@ -58,6 +87,10 @@ public class DefaultCameraController extends Observable implements CameraControl
             camera.stopPreview();
             camera.release();
             camera = null;
+        }
+
+        if(orientationListener != null) {
+            orientationListener.disable();
         }
 
         deleteObservers();
